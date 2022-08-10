ea3c5c0930d8a5b456d78e9a16ae155a73bad095
ASUS <tanghieu7988@gmail.com>
2022-08-10 15:12:37 +0700

    first commit

---
+++ exercise07-bai2.js
@@ -0,0 +1,6 @@
+var inputWidth = prompt("Enter the width");
+var inputHeight = prompt("Enter the height");
+var area = inputWidth*inputHeight;
+document.write("Enter width = "+inputWidth);
+document.write("<br/>Enter height = "+inputHeight);
+document.write("<br/>area="+area);
